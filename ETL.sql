2182b0aa67b0617036d0c6d34bce94cdf2607253
chriscapistrano <41343988+chriscapistrano@users.noreply.github.com>
2018-11-14 17:04:51 -0700

    Add files via upload

---
+++ ETL.sql
@@ -0,0 +1,22 @@
+use yelp_ETL;
+
+SET SQL_SAFE_UPDATES = 0;
+    
+ create table users_new1 (
+	user_id varchar(200) PRIMARY KEY,
+    review_count INT,
+    useful iNT,
+    funny INT,
+    fans INT
+    );    
+    
+create table reviews_new1 (
+		user_id varchar(200),
+        stars INT
+        );
+        
+select * from users_new1;
+select * from reviews_new1;
+
+select u.user_id, review_count, useful, funny, fans, r.stars  from users_new1 AS u
+	join reviews_new1 AS r ON u.user_id = r.user_id;
